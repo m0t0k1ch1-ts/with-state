b2b060663b1a3708876249ab3e6d45ae1f83c491
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2025-04-15 19:54:16 +0900

    feat: implement MaybeInitialized

---
+++ src/maybe-initialized.ts
@@ -0,0 +1,22 @@
+export type MaybeInitialized<T> = Initialized<T> | Uninitialized;
+
+export type Initialized<T> = {
+  data: T;
+  isInitialized: true;
+};
+
+export type Uninitialized = {
+  isInitialized: false;
+};
+
+export function maybeInitialized<T>(data?: T): MaybeInitialized<T> {
+  return data !== undefined ? initialized(data) : uninitialized();
+}
+
+export function initialized<T>(data: T): Initialized<T> {
+  return { data, isInitialized: true };
+}
+
+export function uninitialized(): Uninitialized {
+  return { isInitialized: false };
+}
